63192316469686ba263e43f3b219fe3913ee3316
Robin Foutel <robin.foutel@gmail.com>
2023-09-04 09:17:34 +0200

    Installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
+
+const exclusionArray = [
+  '/login',
+  '/register',
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex flex-col">
+      {/*{exclusionArray.indexOf(location.pathname) < 0 && <Header/>}*/}
+      <div className="flex flex-1 overflow-y-auto">
+        <Routes>
+          <Route path="/" >
+            <Route index element={<Home />} />
+            {/*<Route path="/login" element={<Login />} />*/}
+            {/*<Route path="/register" element={<Register />} />*/}
+            <Route path="/*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </div>
     </div>
   );
 }
